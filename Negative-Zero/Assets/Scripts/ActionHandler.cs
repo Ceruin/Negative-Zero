eede7a6700f4ecdab7ac042af6c363d2ffaefc13
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-06-21 19:36:43 -0400

    Yes
    Turned it into an RNG Calculator

---
+++ Negative-Zero/Assets/Scripts/ActionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionHandler : MonoBehaviour
+{
+    [SerializeField] TMP_Text output;
+    [SerializeField] TMP_Text currentList;
+    [SerializeField] TMP_InputField inputField;
+
+    System.Random random = new System.Random();
+    string input = string.Empty;
+
+    public void StoreText()
+    {
+        currentList.text = input = inputField.text;
+    }
+
+    public void GetRandom()
+    {
+        var test = input.Split('\n');
+
+        int index = random.Next(0, test.Length);
+
+        output.text = test[index];
+    }
+}
---
+++ Negative-Zero/Assets/Scripts/TextFix.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextFix : MonoBehaviour
+{
+    TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        text.outlineColor = Color.black;
+        text.outlineWidth = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
